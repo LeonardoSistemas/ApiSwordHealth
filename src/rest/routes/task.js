44626ced21cfe9defb003d8e748cdac18d16779d
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-13 15:28:53 -0300

    feat(route): adding validation to insert and update task

--- src/rest/routes/task.js
+++ src/rest/routes/task.js
@@ -4,15 +4,20 @@ const routes = Router();
 const TaskController = require("../controllers/task");
 const taskController = new TaskController();
 
+const {    
+    PostCreateTask
+    , PutUpdateTask
+  } = require("../request-validation/request-validation");
+
 routes.get("/task", (req, res, next) => {
     taskController.consultTask(req, res, next);
 });
 
-routes.post("/task", (req, res, next) => {
+routes.post("/task", PostCreateTask, (req, res, next) => {
     taskController.insertTask(req, res, next);
 });
 
-routes.put("/task/:id", (req, res, next) => {
+routes.put("/task/:id", PutUpdateTask, (req, res, next) => {
     taskController.updateTask(req, res, next);
 });
 
