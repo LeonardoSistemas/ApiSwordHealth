2140f1683cc6a15993e4acd3ea1b142655807d87
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-13 01:04:35 -0300

    feat(service): added method to change task

--- src/business/services/task.js
+++ src/business/services/task.js
@@ -1,4 +1,4 @@
-const { BadRequest } = require("../../common/ExceptionHandler");
+const { BadRequest, NotFoundError } = require("../../common/ExceptionHandler");
 const TaskRepository = require("../repository/task");
 
 class TaskService {
@@ -9,7 +9,7 @@ class TaskService {
   async consultTask () {
     const resultConsultTask = await this.taskRepository.consultTask();
     return resultConsultTask;
-  }
+  }  
 
   async insertTask (objectTask) {
 
@@ -18,6 +18,20 @@ class TaskService {
       return new BadRequest("Insert Task Services | the task object is not in a correct format").message;
     const resultInsertTask = await this.taskRepository.insertTask(objectTask);
     return resultInsertTask;
+  }
+
+  async updateTask (objectTask) {
+
+    const returnValidateObject = await this.validateObjectTask(objectTask);
+    if(!returnValidateObject)
+      return new BadRequest("Update Task Services | the task object is not in a correct format").message;
+    
+    const returnConsultTaskByID = await this.taskRepository.consultTaskByID(objectTask)
+    if(!returnConsultTaskByID)
+      return new NotFoundError("Update Task Services | no tasks found for this id").message;
+
+    const resultUpdateTask = await this.taskRepository.updateTask(objectTask);
+    return resultUpdateTask;
   }
 
   async validateObjectTask (objectTask){
