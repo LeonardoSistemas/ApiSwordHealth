f54194e0a0168adf28fe38edfbbdbf67bf22593a
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-12 22:56:47 -0300

    feat(repository): adding method to insert tasks

--- src/business/repository/task.js
+++ src/business/repository/task.js
@@ -1,5 +1,6 @@
 const { BadRequest } = require("../../common/ExceptionHandler");
 
+let arrayTask = [{id:1, description:"Desenvolver API"},{id:2, description:"Desenvolver API Sword"}]
 class TaskRepository {
   constructor () {
     
@@ -7,7 +8,25 @@ class TaskRepository {
 
   async consultTask () {
     
-    return [{id:1, description:"Desenvolver API"},{id:2, description:"Desenvolver API Sword"}];
+    return arrayTask;
+  }
+
+  async insertTask (objectTask) {
+
+    const returnValidateObject = await this.validateObjectTask(objectTask);
+    if(!returnValidateObject)
+      return new BadRequest("Insert Task Repository | the task object is not in a correct format").message;
+    
+    arrayTask.push(objectTask)
+    return {rowAffect: 1};
+  }
+
+  async validateObjectTask (objectTask){
+    const { description } = objectTask
+      if (!description) {
+        return false
+      }
+      return true
   }
 }
 
