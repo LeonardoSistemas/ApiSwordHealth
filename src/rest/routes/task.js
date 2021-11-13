44a5683ce87bbce2b0bc18384b7df94e2405e33b
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-12 22:55:34 -0300

    feat: adding route to insert tasks

--- src/rest/routes/task.js
+++ src/rest/routes/task.js
@@ -8,4 +8,9 @@ routes.get("/task", (req, res, next) => {
     taskController.consultTask(req, res, next);
 });
 
+routes.post("/task", (req, res, next) => {
+    taskController.insertTask(req, res, next);
+});
+
+
 module.exports = routes;
