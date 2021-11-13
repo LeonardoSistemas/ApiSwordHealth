b2351401aef19c7b9996497dc33bdc5ee4e3985b
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-13 01:04:49 -0300

    feat(repository): added method to change task

--- src/business/repository/task.js
+++ src/business/repository/task.js
@@ -1,32 +1,61 @@
-const { BadRequest } = require("../../common/ExceptionHandler");
+const { BadRequest, NotFoundError } = require("../../common/ExceptionHandler");
 
-let arrayTask = [{id:1, description:"Desenvolver API"},{id:2, description:"Desenvolver API Sword"}]
+let arrayTask = [{ id: 1, description: "Desenvolver API" }, { id: 2, description: "Desenvolver API Sword" }]
 class TaskRepository {
-  constructor () {
-    
+  constructor() {
+
   }
 
-  async consultTask () {
-    
+  async consultTask() {
+
     return arrayTask;
   }
 
-  async insertTask (objectTask) {
+  async consultTaskByID(objectTask) {
 
-    const returnValidateObject = await this.validateObjectTask(objectTask);
-    if(!returnValidateObject)
-      return new BadRequest("Insert Task Repository | the task object is not in a correct format").message;
-    
-    arrayTask.push(objectTask)
-    return {rowAffect: 1};
+    let findTask = false;
+
+    arrayTask.forEach((elemento) => {
+      if (elemento.id === objectTask.id)
+        findTask = true
+    })
+    return findTask;
   }
 
-  async validateObjectTask (objectTask){
+  async insertTask(objectTask) {
+
+    const returnValidateObject = await this.validateObjectTask(objectTask);
+    if (!returnValidateObject)
+      return new BadRequest("Insert Task Repository | the task object is not in a correct format").message;
+
+    arrayTask.push(objectTask)
+    return { rowAffect: 1 };
+  }
+
+  async updateTask(objectTask) {
+
+    const returnValidateObject = await this.validateObjectTask(objectTask);
+    if (!returnValidateObject)
+      return new BadRequest("Update Task Repository | the task object is not in a correct format").message;
+
+    const returnConsultTaskByID = await this.consultTaskByID(objectTask)
+    if (!returnConsultTaskByID)
+      return new NotFoundError("Update Task Repository | no tasks found for this id").message;
+
+    arrayTask.forEach((elemento, indice) => {
+      if (elemento.id === objectTask.id)
+        arrayTask.splice(indice, 1)
+    })
+    arrayTask.push(objectTask)
+    return { rowAffect: 1 };
+  }
+
+  async validateObjectTask(objectTask) {
     const { description } = objectTask
-      if (!description) {
-        return false
-      }
-      return true
+    if (!description) {
+      return false
+    }
+    return true
   }
 }
 
