cc4c8c5d26816d6de7bbabfe903d232d52b6891b
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-12 22:57:16 -0300

    test(service): adding method to insert tasks

--- __tests__/business/services/taskService.spec.js
+++ __tests__/business/services/taskService.spec.js
@@ -20,8 +20,41 @@ describe("TaskService", () => {
 
       expect(result).toBe(taskRepositoryReturnMock);
     });
-  });
-  /* describe("- ERROR CASES -", () => {
+
+    it("insertTask | Ensure that the task was successfully inserted", async () => {
+      const objectTask = {
+        id: 3,
+        description: "Test API"
+      };
+
+      const taskRepositoryReturnMock = {
+        rowAffect: 1
+      };
+
+      const createTaskRepositoryMock = () => taskRepositoryReturnMock;
+
+      jest.spyOn(
+        taskService.taskRepository,
+        "insertTask")
+        .mockImplementationOnce(createTaskRepositoryMock);
+      const result = await taskService.insertTask(objectTask);
+
+      expect(result).toBe(taskRepositoryReturnMock);
+    });
     
-  }); */
+  });
+  describe("- ERROR CASES -", () => {
+    it("insertTask | Ensure object validation failed", async () => {
+      const objectTask = {
+        id: 3
+      };
+
+      try {
+        const result = await taskService.insertTask(objectTask);
+      } catch (error) {        
+        expect(error.name).toBe("BadRequest");
+        expect(error.message).toBe("Insert Task Services | the task object is not in a correct format");
+      }      
+    });
+  });
 });
