5882086ce89be011448c08194eb55387a9b61e79
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-12 22:57:32 -0300

    test(repository): adding method to insert tasks

--- __tests__/business/repository/taskRepository.spec.js
+++ __tests__/business/repository/taskRepository.spec.js
@@ -10,8 +10,30 @@ describe("TaskRepository", () => {
 
       expect(result).toBeTruthy();
     });
+
+    it("insetTask | Ensure that the task was successfully inserted", async () => {
+      const objectTask = {
+        id: 3,
+        description: "Test API"
+      };
+     
+      const result = await taskRepository.insertTask(objectTask);
+
+      expect(result).toBeDefined();
+    });
   });
-  /* describe("- ERROR CASES -", () => {
-    
-  }); */
+  describe("- ERROR CASES -", () => {
+    it("insertTask | Ensure object validation failed", async () => {
+      const objectTask = {
+        id: 3
+      };
+
+      try {
+        const result = await taskRepository.insertTask(objectTask);
+      } catch (error) {        
+        expect(error.name).toBe("BadRequest");
+        expect(error.message).toBe("Insert Task Repository | the task object is not in a correct format");
+      }      
+    });
+  });
 });
