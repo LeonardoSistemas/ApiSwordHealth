7761dfb78ab98ab4369075829b27ead7fd2ff39f
Leonardo Treviso <leonardosistemas@outlook.com>
2021-11-15 15:08:19 -0300

    refactor: adding the userid field in the validation of the route

--- src/rest/request-validation/PostCreateTask.js
+++ src/rest/request-validation/PostCreateTask.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const postCreateTaskSchema = Joi.object({
-  description: Joi.string().required().max(2500)
+  description: Joi.string().required().max(2500),
+  userId: Joi.number().required()
 });
 module.exports = { postCreateTaskSchema };
